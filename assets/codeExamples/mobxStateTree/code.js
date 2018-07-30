fa489bfc07c38a129b36820fd41919b70274c15b
Max Gallo <max.gallo@dazn.com>
2018-07-30 20:47:17 +0100

    shorter mobx-state-tree example

--- assets/codeExamples/mobxStateTree/code.js
+++ assets/codeExamples/mobxStateTree/code.js
@@ -1,25 +1,22 @@
-import {observer} from 'mobx-react';
-import React, {Component} from 'react';
+import { observer } from 'mobx-react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { types } from 'mobx-state-tree';
 
 const CarStore = types
     .model('Car', {
-        name: types.string,
+        name: types.string
     })
     .views(self => ({
-        get isFerrari() { return self.name.includes('Ferrari') }
-    }))
+        get isFerrari() {
+            return self.name.includes('Ferrari');
+        }
+    }));
 
 const CarParkStore = types
     .model('CarPark', {
-        cars: types.array(CarStore),
+        cars: types.array(CarStore)
     })
-    .views(self => ({
-        get howManyFerrari(){
-            return self.cars.filter(car => car.isFerrari).length;
-        }
-    }))
     .actions(self => ({
         addCar(car) {
             self.cars.push(car);
@@ -28,23 +25,25 @@ const CarParkStore = types
 
 @observer
 class CarParkView extends Component {
-    renderCar(car){ return <li key={car.name}>{car.name}</li> }
+    renderCar(car) {
+        return <li key={car.name}>{car.name}</li>;
+    }
     render() {
-        const { carPark } = this.props
-        return (<div>
-            <ul>{ carPark.cars.map(this.renderCar) } </ul>
-            How many Ferrari? { carPark.howManyFerrari }
-        </div>);
+        const { carPark } = this.props;
+        return (
+            <div>
+                <ul>{carPark.cars.map(this.renderCar)} </ul>
+            </div>
+        );
     }
 }
 
-const carParkStore = CarParkStore.create({ cars: ['Fiat 500']});
+const carParkStore = CarParkStore.create({ cars: [{ name: 'Fiat 500' }] });
 
 ReactDOM.render(
     <CarParkView carPark={carParkStore} />,
     document.getElementById('root')
-)
+);
 
 carParkStore.addCar({ name: 'Ferrari 458 Italia' });
 carParkStore.addCar({ name: 'Ferrari Enzo' });
-
